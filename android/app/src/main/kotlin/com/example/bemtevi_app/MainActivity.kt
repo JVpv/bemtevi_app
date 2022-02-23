965060183a78203ddd2421bcb323979bb1f2400e
JV Prado <jose.vpv7@gmail.com>
2022-02-23 19:26:56 -0300

    Initial commit, created base project structure

---
+++ android/app/src/main/kotlin/com/example/bemtevi_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bemtevi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bemtevi_app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      title: 'Bemtevi',
+      home: const App(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
