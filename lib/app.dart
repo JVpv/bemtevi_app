b851798a3ead06f03bb470ce63443761bca51713
JV Prado <jose.vpv7@gmail.com>
2022-02-24 22:24:25 -0300

    Added OAuth2 discord screen. Will take some time to study better design patterns before continuing here.

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,42 @@
+import 'package:bemtevi_app/pages/home/home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:visa/auth-data.dart';
+import 'package:visa/discord.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    onDone(AuthData authData) {
+      print('Logged in!');
+      print(authData);
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => HomeScreen(authData: authData)));
+      //TODO: Write redirect to main screen
+    }
+
+    final clientID =
+        dotenv.env['bemtevi_client_id']; //OAuth client ID (developer portal)
+    final clientSecret =
+        dotenv.env['bemtevi_client_secret']; //OAuth secret (developer portal)
+    final redirectUri = dotenv
+        .env['bemtevi_redirect_uri']; //OAuth redirect url (developer portal)
+    final scope =
+        dotenv.env['bemtevi_app_scopes']; //OAuth scopes (that the url requires)
+    final state = 'discordAuth'; //OAuth state (whatever you want)
     return Scaffold(
-      body: Container(
-        color: Colors.teal,
-      ),
+      body: DiscordAuth().visa.authenticate(
+          clientID: clientID,
+          clientSecret: clientSecret,
+          redirectUri: redirectUri,
+          state: state,
+          scope: scope,
+          onDone: onDone),
     );
   }
 }
---
+++ lib/pages/home/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:visa/auth-data.dart';
+
+class HomeScreen extends StatelessWidget {
+  final AuthData authData;
+  const HomeScreen({Key? key, required this.authData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
